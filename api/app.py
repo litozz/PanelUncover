16d7ce58669221618454af1c7e9aaa7eb48a18f1
Carlos Martinez <jcarlosmarv@gmail.com>
2020-10-12 15:50:38 +0200

    Add wsgi entrypoint

--- api/app.py
+++ api/app.py
@@ -10,7 +10,3 @@ def index():
         return 'Welcome to PanelUncover game'
     elif flask.request.method == 'PUT':
         return reveal()
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
