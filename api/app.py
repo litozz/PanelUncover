5f2d881b87df3bcb554a0b2b065aea955dcb245f
Carlos Martinez <jcarlosmarv@gmail.com>
2020-10-12 17:33:37 +0200

    App flow based on backend checks

--- api/app.py
+++ api/app.py
@@ -2,11 +2,28 @@
 import flask
 from utils import reveal
 
-app = flask.Flask(__name__)
+secrets={
+    '3456':1,
+    '4567':2,
+    '4730':3,
+    '6459':4,
+    '6132':5,
+    '0584':6,
+    '6153':7,
+    '6492':8,
+    '9784':9
+}
 
-@app.route('/', methods=['GET', 'PUT'])
+app = flask.Flask(__name__, template_folder='../static/html')
+solved = []
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if flask.request.method == 'GET':
-        return 'Welcome to PanelUncover game'
-    elif flask.request.method == 'PUT':
-        return reveal()
+        return flask.render_template('index.html', secrets=secrets, solved=solved)
+    elif flask.request.method == 'POST':
+        panel_id = secrets.get(flask.request.form.get('text_code'))
+        if panel_id is not None:
+            solved.append(panel_id)
+        return flask.render_template('index.html', secrets=secrets, solved=solved)
